fbbd665bad8e04e4c1ca3beeb6fa3cbb90a39552
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-05-25 16:53:27 -0500

    yep

---
+++ magic.js
@@ -0,0 +1,10 @@
+var names = ["james", "kyle", "jordan", "Jane Doe"]
+var editProfile = document.getElementByID('#editButton')
+var jane = document.getElementByID('#Jane')
+
+function nameSwap() {
+    for (i = 0; i < names.length; i++) {
+        jane == math.floor(math.random(names.length.name[i]))
+        console.log(jane)
+    }
+}
